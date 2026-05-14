e072b1d0d1c07c950cf659f02852442691e213e8
alinaresblanco-gif <alinares.blanco@gmail.com>
2026-05-14 10:40:09 +0200

    cargar

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+// service-worker.js
+// Forzar siempre la última versión de los archivos (estrategia network-first)
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
